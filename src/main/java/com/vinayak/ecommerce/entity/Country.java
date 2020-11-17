4529296fb99a84290884b75eae4c7bc5887c9b14
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-17 22:00:57 +0530

    Added Country Entity

---
+++ src/main/java/com/vinayak/ecommerce/entity/Country.java
@@ -0,0 +1,28 @@
+package com.vinayak.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="code")
+    private String code;
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+}
