8e5330413fe8f2ebee244491d15d0e8c29056946
Vinayak <vinayakchaurasia99@gmail.com>
2021-04-19 00:16:48 +0530

    Added controller for order checkout

---
+++ src/main/java/com/vinayak/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,27 @@
+package com.vinayak.ecommerce.controller;
+
+import com.vinayak.ecommerce.dto.Purchase;
+import com.vinayak.ecommerce.dto.PurchaseResponse;
+import com.vinayak.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService){
+        this.checkoutService=checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+}
