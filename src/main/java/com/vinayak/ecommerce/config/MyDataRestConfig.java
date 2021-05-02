928d2f9494a22158148affcd274c838c9c0dac5a
Vinayak <vinayakchaurasia99@gmail.com>
2021-05-02 17:38:52 +0530

    Added patch as unsupported method

--- src/main/java/com/vinayak/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/vinayak/ecommerce/config/MyDataRestConfig.java
@@ -31,7 +31,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         // disable HTTP methods for Product: PUT, POST and DELETE
         disableHttpMethods(Product.class,config, theUnsupportedActions);
