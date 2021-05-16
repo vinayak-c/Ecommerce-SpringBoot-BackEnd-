359dd67f8645e17d596e1b88ef5c2edb7b839526
Vinayak <vinayakchaurasia99@gmail.com>
2021-05-16 23:37:34 +0530

    Secured order endpoint and disabled csrf

---
+++ src/main/java/com/vinayak/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.vinayak.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        //protect endpoints api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        //cors filters
+        http.cors();
+
+        //Forcing a non-empty response body for 401's to make response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        http.csrf().disable();
+
+
+    }
+}
