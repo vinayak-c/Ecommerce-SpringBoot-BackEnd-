9849ce6f574990635921619ce5de42a4af08f678
Vinayak <vinayakchaurasia99@gmail.com>
2022-01-02 21:14:54 +0530

    Added swagger

--- src/main/java/com/vinayak/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/vinayak/ecommerce/SpringBootEcommerceApplication.java
@@ -1,9 +1,12 @@
 package com.vinayak.ecommerce;
 
+import com.vinayak.ecommerce.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class SpringBootEcommerceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/vinayak/ecommerce/config/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package com.vinayak.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket EcommerceAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("E-Commerce API")
+                .version("1.0")
+                .description("Backend API for the Spring Boot E-commerce Application")
+                .contact(new Contact("Vinayak Chaurasia", "https://github.com/vinayak-c", "vinayakchaurasia99@gmail.com"))
+                .build();
+    }
+
+}
